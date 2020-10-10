0850399bed09b39175eb7770974506db79cf117d
zavanton <zavanton123@gmail.com>
2020-10-10 12:31:10 +0300

    admin pages are updated, debug toolbar and ckeditor added

--- blog/models.py
+++ blog/models.py
@@ -28,15 +28,15 @@ class Tag(models.Model):
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, verbose_name='Название')
     slug = models.SlugField(max_length=255, verbose_name='Url', unique=True)
-    author = models.CharField(max_length=100)
-    content = models.TextField(blank=True)
+    author = models.CharField(max_length=100, verbose_name='Автор')
+    content = models.TextField(blank=True, verbose_name='Содержание')
     created_at = models.DateField(auto_now_add=True, verbose_name='Опублиновано')
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True, width_field=300, height_field=200)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True, verbose_name='Фото')
     views = models.IntegerField(default=0, verbose_name='Кол-во просмотров')
     category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='posts')
-    tags = models.ManyToManyField(Tag, blank=True, related_name='posts')
+    tags = models.ManyToManyField(Tag, blank=True, related_name='posts', verbose_name='Тэги')
 
     def __str__(self):
         return self.title
--- dart/urls.py
+++ dart/urls.py
@@ -1,7 +1,17 @@
+import debug_toolbar
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns = [
+                      path('__debug__/', include(debug_toolbar.urls)),
+                  ] + urlpatterns
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
