46c9b7fbb4391073bff8b726cc51a2b0de413749
zavanton <zavanton123@gmail.com>
2020-10-10 10:51:05 +0300

    templates added

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("hello")
+    return render(request, 'blog/index.html', context={'name': 'zavanton'})
