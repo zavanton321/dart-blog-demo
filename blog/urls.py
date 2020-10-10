118c0c19e0a89dbad8ab7363379559a578b72845
zavanton <zavanton123@gmail.com>
2020-10-10 13:12:21 +0300

    header and footer menus are added (and cached)

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from blog.views import index
+from blog.views import index, get_category
 
 urlpatterns = [
     path('', index, name='home'),
+    path('category/<str:slug>', get_category, name='category')
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'blog/index.html', context={'name': 'zavanton'})
+
+
+def get_category(request, slug):
+    return render(request, 'blog/category.html', context={'the_slug': slug})
