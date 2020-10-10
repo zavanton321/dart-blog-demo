5c23b839608e518677aefca24fe11aec57985159
zavanton <zavanton123@gmail.com>
2020-10-10 14:06:46 +0300

    active page is indicated in the header menu

--- blog/static/js/active.js
+++ blog/static/js/active.js
@@ -1,21 +1,15 @@
 $(document).ready(function () {
-    // var filterFns = {
-    //     numberGreaterThan50: function () {
-    //         var number = $(this).find('.number').text();
-    //         return parseInt(number, 10) > 50;
-    //     },
-    //     ium: function () {
-    //         var name = $(this).find('.name').text();
-    //         return name.match(/ium$/);
-    //     }
-    // };
-    // $('.filters-button-group').on('click', 'button', function () {
-    //     // var filterValue = $(this).attr('data-filter');
-    //     // filterValue = filterFns[filterValue] || filterValue;
-    //     // $grid.isotope({
-    //     //     filter: filterValue
-    //     // });
-    // });
+
+
+    $('.menu a').each(function () {
+        let location = window.location.protocol + '//' + window.location.host + window.location.pathname;
+        let link = this.href;
+        if (location == link) {
+            $(this).parent().addClass('active')
+        }
+    });
+
+
     $('.button-group').each(function (i, buttonGroup) {
         var $buttonGroup = $(buttonGroup);
         $buttonGroup.on('click', 'button', function () {
--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from blog.views import index, get_category
 
 urlpatterns = [
     path('', index, name='home'),
-    path('category/<str:slug>', get_category, name='category')
+    path('category/<str:slug>/', get_category, name='category')
 ]
